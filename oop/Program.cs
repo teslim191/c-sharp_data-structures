847ce22bde6ebbe5a479a1b20ae6172bad340cf0
Teslim Jimoh <teslimjimoh191@gmail.com>
2023-06-28 18:11:08 +0100

    refreshing my knowledge of oop

---
+++ oop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace oop
+{
+
+    class Car
+    {
+        string color;
+        int maxSpeed;
+
+        public void drivingSchool()
+        {
+            Console.WriteLine("what a nice school to learn");
+        }
+
+        static void Main(string[] args)
+        {
+            Car MyCar = new Car();
+            MyCar.color = "Red";
+            MyCar.maxSpeed = 200;
+
+            Console.WriteLine(MyCar.color);
+            Console.WriteLine(MyCar.maxSpeed);
+            MyCar.drivingSchool();
+
+        }
+    }
+
+
+
+}
---
+++ oop2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car MyCar = new Car();
+            
+            Console.WriteLine(MyCar.color);
+
+        }
+    }
+}
