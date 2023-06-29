21dcd6690eaa9b9256917c3d90e913de7b125bf9
Teslim Jimoh <teslimjimoh191@gmail.com>
2023-06-29 14:10:33 +0100

    worked with encapsulation and inheritance

--- oop2/Program.cs
+++ oop2/Program.cs
@@ -3,13 +3,30 @@
 
 namespace oop2
 {
-    class Program
+    class MyClass
     {
+        private string home;
+    }
+    class House
+    {
+        int windows;
+        bool duplex;
+        public House(int numbers, bool type)
+        {
+            windows = numbers;
+            duplex = type;
+
+        }
+
+        public void annualRent(int numbers)
+        {
+            Console.WriteLine("the house has {0} windows",numbers);
+        }
         static void Main(string[] args)
         {
-            Car MyCar = new Car();
-            
-            Console.WriteLine(MyCar.color);
+            House house_1 = new House(4, true);
+            Console.WriteLine(house_1.windows);
+            house_1.annualRent(4);
 
         }
     }
---
+++ oop3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace oop3
+{
+    class Person
+    {
+        public string Name
+        {
+            get; set;
+        }
+
+       /* public Person(string value)
+        {
+            Name = value;
+        }*/
+        public void eating(string name)
+        {
+            Console.WriteLine("{0} is eating",name);
+        }
+    }
+    //inheritance
+    class FirstPerson: Person
+    {
+
+    }
+
+    class Animal
+    {
+        //to override the base class
+        public virtual void animalSound()
+        {
+            Console.WriteLine("the animal made a sound");
+        }
+
+    }
+
+    class Dog : Animal
+    {
+        //to allow the derived class override base class
+        public override void animalSound()
+        {
+            Console.WriteLine("the dog is barking");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("the cat is meowing");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person_1 = new Person();
+            //person_1.Name = "john";
+
+            //Console.WriteLine(person_1.Name);
+
+            FirstPerson f_person = new FirstPerson();
+            f_person.Name = "john";
+            f_person.eating("tayo");
+
+            Animal animal = new Animal();
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myCat.animalSound();
+        }
+    }
+}
